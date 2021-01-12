44a726e04e64b020a048f1ed816e3a130a8e70e7
okayu1230z <okayu000z@yahoo.co.jp>
2021-01-12 11:47:23 +0900

    minor update

--- app/controllers/ror_controller.rb
+++ app/controllers/ror_controller.rb
@@ -7,7 +7,6 @@ class RorController < ApplicationController
     con = ActiveRecord::Base.connection
     result = con.select_all("SELECT * FROM users WHERE nickname = '#{params[:nickname]}' AND password = '#{params[:password]}'")
 
-    puts result
     if result.to_a.size == 1
       render plain: 'FLAG{ror_sqli}'
       return
